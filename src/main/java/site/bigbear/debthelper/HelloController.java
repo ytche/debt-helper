4e47c77037fb30499a1836ab152162ed70dd55b3
ytche <cheyantao@foxmail.com>
2019-07-29 17:13:13 +0800

    project init

---
+++ src/main/java/site/bigbear/debthelper/HelloController.java
@@ -0,0 +1,21 @@
+package site.bigbear.debthelper;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author cheyantao
+ * @date 2019-07-29
+ */
+@Controller
+@RequestMapping("/api/v1/hello")
+public class HelloController {
+
+    @ResponseBody
+    @GetMapping
+    public String hello(){
+        return "hello";
+    }
+}
