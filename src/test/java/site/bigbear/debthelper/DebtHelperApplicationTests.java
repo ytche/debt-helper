67e66104d8aa5f4b7f935985f0de2add59f1ce20
ytche <cheyantao@foxmail.com>
2019-08-01 16:28:21 +0800

    test

--- src/test/java/site/bigbear/debthelper/DebtHelperApplicationTests.java
+++ src/test/java/site/bigbear/debthelper/DebtHelperApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 import site.bigbear.debthelper.model.TestModel;
 import site.bigbear.debthelper.repository.TestRepository;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableTransactionManagement
 public class DebtHelperApplicationTests {
   @Autowired private TestRepository testRepository;
 
